2dc2c3c96de824afff4a7d5c1ddec464b972aa52
liujj <liujj@rd.netease.com>
2016-08-11 17:39:23 +0800

    first commit

---
+++ RuntimeTest/UIView+Gesture.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Gesture.h
+//  RuntimeTest
+//
+//  Created by LiuJinjun on 16/8/11.
+//  Copyright © 2016年 NetEase Youdao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Gesture)
+
+@end
---
+++ RuntimeTest/UIView+Gesture.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Gesture.m
+//  RuntimeTest
+//
+//  Created by LiuJinjun on 16/8/11.
+//  Copyright © 2016年 NetEase Youdao. All rights reserved.
+//
+
+#import "UIView+Gesture.h"
+
+@implementation UIView (Gesture)
+
+@end
